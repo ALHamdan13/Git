b360725dc27bcc28773cc87084655c19d912c453
ALHamdan13 <72266701+ALHamdan13@users.noreply.github.com>
2021-01-13 08:31:28 -0600

    Add files via upload

---
+++ Git/Git/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git
+//
+//  Created by A.M. Student on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Get Hub!")
+            .padding()
+        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git/Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by A.M. Student on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
